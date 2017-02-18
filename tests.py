ff9fca4c453ce423152ee6088b2b89740077267d
Christian Heimes <christian@python.org>
2017-02-18 11:36:22 +0100

    Add test cases from t_ldap_schema_tokenizer.py plus additional edge cases
    Signed-off-by: Christian Heimes <christian@python.org>

--- tests.py
+++ tests.py
@@ -12,23 +12,53 @@ from fast_split_tokens import fast_split_tokens
 
 class SplitTokensTest(unittest.TestCase):
     testfile = 'testcases.jsonl'
-    testcases = None
+    testcases = [
+        # from t_ldap_schema_tokenizer.py
+        (" BLUBBER DI BLUBB ", ["BLUBBER", "DI", "BLUBB"]),
+        ("BLUBBER DI BLUBB", ["BLUBBER", "DI", "BLUBB"]),
+        ("BLUBBER  DI   BLUBB  ", ["BLUBBER", "DI", "BLUBB"]),
+        ("BLUBBER  DI  'BLUBB'   ", ["BLUBBER", "DI", "BLUBB"]),
+        ("BLUBBER ( DI ) 'BLUBB'   ", ["BLUBBER", "(", "DI", ")", "BLUBB"]),
+        ("BLUBBER(DI)", ["BLUBBER", "(", "DI", ")"]),
+        ("BLUBBER ( DI)", ["BLUBBER", "(", "DI", ")"]),
+        ("BLUBBER ''", ["BLUBBER", ""]),
+        ("( BLUBBER (DI 'BLUBB'))",
+         ["(", "BLUBBER", "(", "DI", "BLUBB", ")", ")"]),
+        ("BLUBB (DA$BLAH)", ['BLUBB', "(", "DA", "BLAH", ")"]),
+        ("BLUBB ( DA $  BLAH )", ['BLUBB', "(", "DA", "BLAH", ")"]),
+        ("BLUBB (DA$ BLAH)", ['BLUBB', "(", "DA", "BLAH", ")"]),
+        ("BLUBB (DA $BLAH)", ['BLUBB', "(", "DA", "BLAH", ")"]),
+        ("BLUBB 'DA$BLAH'", ['BLUBB', "DA$BLAH"]),
+        ("BLUBB DI 'BLU B B ER' DA 'BLAH' ",
+         ['BLUBB', 'DI', 'BLU B B ER', 'DA', 'BLAH']),
+        ("BLUBB DI 'BLU B B ER' DA 'BLAH' LABER",
+         ['BLUBB', 'DI', 'BLU B B ER', 'DA', 'BLAH', 'LABER']),
+        # more
+        ("BLUBB '(DA$BLAH)'", ['BLUBB', "(DA$BLAH)"]),
+        ("BLUBB '(DA)'", ['BLUBB', "(DA)"]),
+        ("BLUBB '(DA BLAH)'", ['BLUBB', "(DA BLAH)"]),
+        ("BLUBB 'BLAH \"DA\" BLUBB'", ['BLUBB', 'BLAH \"DA\" BLUBB']),
+        ("BLUBB\t'DA\tBLUB'", ['BLUBB', "DA\tBLUB"]),
+    ]
+
     maxDiff = None
     loops = 10
 
     @classmethod
     def setUpClass(cls):
-        testcases = []
+        testcases = cls.testcases
         with open(cls.testfile) as f:
             for line in f:
                 testcases.append(json.loads(line))
-        cls.testcases = testcases
 
     def test_split_tokens(self):
         start = time.time()
         for i in range(self.loops):
             for case, expected in self.testcases:
                 result = split_tokens(case, {})
+                # XXX split_token() doesn't consider \t white space
+                if '\t' in case:
+                    continue
                 self.assertEqual(expected, result)
         print(time.time() - start)
 
@@ -37,7 +67,9 @@ class SplitTokensTest(unittest.TestCase):
         for i in range(self.loops):
             for case, expected in self.testcases:
                 result = fast_split_tokens(case, {})
-                self.assertEqual(expected, result)
+                if expected != result:
+                    print(case)
+                    self.assertEqual(expected, result)
         print(time.time() - start)
 
 
